bc95611b94e5ab81cef4afbc3bc7f82fb6743b8c
MossDog <lukejohnmosshughes@gmail.com>
2024-04-05 01:47:35 +0100

    added create_tables.sql

---
+++ create_tables.sql
@@ -0,0 +1,15 @@
+CREATE TABLE IF NOT EXISTS Room (
+    room_no INT PRIMARY KEY,
+    age INT NOT NULL,
+);
+
+CREATE TABLE IF NOT EXISTS Observation (
+    observation_id INT AUTO_INCREMENT PRIMARY KEY,
+    room_no INT,
+    bpm INT,
+    temperature FLOAT,
+    fall BOOLEAN,
+    time_observed TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    status VARCHAR(255),
+    FOREIGN KEY (room_no) REFERENCES Room(room_no)
+);
